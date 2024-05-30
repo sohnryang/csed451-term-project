3ab90dca9b4d0fd0a07b2e20db965209163a1df2
Ryang Sohn <loop.infinitely@gmail.com>
2024-05-30 12:54:51 +0900

    refactor(camera): factor out camera config

--- include/camera.hh
+++ include/camera.hh
@@ -8,19 +8,23 @@
 
 #include <glm/glm.hpp>
 
+struct CameraConfig {
+  float aspect_ratio;
+  int image_width;
+  int samples_per_pixel;
+  int max_depth;
+  float vfov;
+  glm::vec3 eye;
+  glm::vec3 center;
+  glm::vec3 up;
+  float defocus_angle;
+  float focus_dist;
+};
+
 class Camera {
 private:
-  float _aspect_ratio;
-  int _image_width;
-  int _samples_per_pixel;
+  CameraConfig _config;
   int _image_height;
-  int _max_depth;
-  float _vfov;
-  float _defocus_angle;
-  float _focus_dist;
-  glm::vec3 _eye;
-  glm::vec3 _center;
-  glm::vec3 _up;
   glm::vec3 _pixel00_location;
   glm::vec3 _pixel_delta_u;
   glm::vec3 _pixel_delta_v;
@@ -39,16 +43,17 @@ private:
   Ray _ray_at_pixel(int y, int x) const;
 
 public:
+  static constexpr CameraConfig DEFAULT_CONFIG = {
+      16.0f / 9.0f, 400,        100,       50,   90.0f,
+      {0, 0, 0},    {0, 0, -1}, {0, 1, 0}, 0.0f, 10.0f};
+
   Camera();
   Camera(const Camera &) = default;
   Camera(Camera &&) = default;
   Camera &operator=(const Camera &) = default;
   Camera &operator=(Camera &&) = default;
 
-  Camera(float aspect_ratio, int image_width, int samples_per_pixel,
-         int max_depth, float vfov, const glm::vec3 &eye,
-         const glm::vec3 &center, const glm::vec3 &up, float defocus_angle,
-         float focus_dist);
+  Camera(const CameraConfig &config);
 
   void render_to_file(const std::string &filename, const Hittable &world);
 };
--- src/camera.cc
+++ src/camera.cc
@@ -12,37 +12,30 @@
 
 #include <glm/glm.hpp>
 
-Camera::Camera()
-    : Camera(16.0f / 9.0f, 400, 100, 50, 90.0f, {0, 0, 0}, {0, 0, -1},
-             {0, 1, 0}, 0.0f, 10.0f) {}
+Camera::Camera() : Camera(DEFAULT_CONFIG) {}
 
-Camera::Camera(float aspect_ratio, int image_width, int samples_per_pixel,
-               int max_depth, float vfov, const glm::vec3 &eye,
-               const glm::vec3 &center, const glm::vec3 &up,
-               float defocus_angle, float focus_dist)
-    : _aspect_ratio(aspect_ratio), _image_width(image_width),
-      _samples_per_pixel(samples_per_pixel), _max_depth(max_depth), _vfov(vfov),
-      _defocus_angle(defocus_angle), _focus_dist(focus_dist), _eye(eye),
-      _center(center), _up(up) {
-  _image_height = std::max(static_cast<int>(image_width / aspect_ratio), 1);
+Camera::Camera(const CameraConfig &config) : _config(config) {
+  _image_height =
+      std::max(static_cast<int>(_config.image_width / _config.aspect_ratio), 1);
 
-  const auto theta = glm::radians(_vfov), h = std::tan(theta / 2),
-             viewport_height = 2.0f * h * _focus_dist,
+  const auto theta = glm::radians(_config.vfov), h = std::tan(theta / 2),
+             viewport_height = 2.0f * h * _config.focus_dist,
              viewport_width =
                  viewport_height *
-                 (static_cast<float>(_image_width) / _image_height);
-  const auto w = glm::normalize(_eye - _center),
-             u = glm::normalize(glm::cross(_up, w)), v = glm::cross(w, u),
-             viewport_u = viewport_width * u, viewport_v = -viewport_height * v,
-             viewport_upper_left = _eye - (focus_dist * w) - viewport_u / 2.0f -
-                                   viewport_v / 2.0f;
-  _pixel_delta_u = viewport_u / static_cast<float>(_image_width);
+                 (static_cast<float>(_config.image_width) / _image_height);
+  const auto w = glm::normalize(_config.eye - _config.center),
+             u = glm::normalize(glm::cross(_config.up, w)),
+             v = glm::cross(w, u), viewport_u = viewport_width * u,
+             viewport_v = -viewport_height * v,
+             viewport_upper_left = _config.eye - (_config.focus_dist * w) -
+                                   viewport_u / 2.0f - viewport_v / 2.0f;
+  _pixel_delta_u = viewport_u / static_cast<float>(_config.image_width);
   _pixel_delta_v = viewport_v / static_cast<float>(_image_height);
   _pixel00_location =
       viewport_upper_left + 0.5f * (_pixel_delta_u + _pixel_delta_v);
 
   const auto defocus_radius =
-      _focus_dist * tan(glm::radians(_defocus_angle / 2));
+      _config.focus_dist * tan(glm::radians(_config.defocus_angle / 2));
   _defocus_disk_u = defocus_radius * u;
   _defocus_disk_v = defocus_radius * v;
 }
@@ -64,7 +57,7 @@ glm::vec3 Camera::_sample_square() const {
 
 glm::vec3 Camera::_defocus_disk_sample() const {
   const auto p = random_in_unit_disk();
-  return _eye + p[0] * _defocus_disk_u + p[1] * _defocus_disk_v;
+  return _config.eye + p[0] * _defocus_disk_u + p[1] * _defocus_disk_v;
 }
 
 Ray Camera::_ray_at_pixel(int y, int x) const {
@@ -73,7 +66,8 @@ Ray Camera::_ray_at_pixel(int y, int x) const {
                  _pixel00_location +
                  (static_cast<float>(x) + offset[0]) * _pixel_delta_u +
                  (static_cast<float>(y) + offset[1]) * _pixel_delta_v,
-             origin = _defocus_angle <= 0 ? _eye : _defocus_disk_sample(),
+             origin = _config.defocus_angle <= 0 ? _config.eye
+                                                 : _defocus_disk_sample(),
              direction = pixel_sample - origin;
   return {origin, direction};
 }
@@ -81,18 +75,18 @@ Ray Camera::_ray_at_pixel(int y, int x) const {
 void Camera::render_to_file(const std::string &filename,
                             const Hittable &world) {
   std::ofstream outfile(filename);
-  outfile << "P3\n" << _image_width << " " << _image_height << "\n255\n";
+  outfile << "P3\n" << _config.image_width << " " << _image_height << "\n255\n";
   for (int y = 0; y < _image_height; y++) {
     std::clog << "\rRemaining scanlines: " << (_image_height - y) << ' '
               << std::flush;
-    for (int x = 0; x < _image_width; x++) {
+    for (int x = 0; x < _config.image_width; x++) {
       auto pixel_color = glm::vec3(0, 0, 0);
-      for (int sample = 0; sample < _samples_per_pixel; sample++) {
+      for (int sample = 0; sample < _config.samples_per_pixel; sample++) {
         const auto ray = _ray_at_pixel(y, x);
         pixel_color += _ray_color(ray, world);
       }
       _write_color(outfile,
-                   pixel_color / static_cast<float>(_samples_per_pixel));
+                   pixel_color / static_cast<float>(_config.samples_per_pixel));
     }
   }
   std::clog << "\rDone.                 \n";
@@ -100,7 +94,7 @@ void Camera::render_to_file(const std::string &filename,
 
 glm::vec3 Camera::_ray_color(const Ray &ray, const Hittable &world,
                              int depth) const {
-  if (depth >= _max_depth)
+  if (depth >= _config.max_depth)
     return {0, 0, 0};
 
   const auto record = world.hit(ray, Interval(0.001f, INFINITY));
